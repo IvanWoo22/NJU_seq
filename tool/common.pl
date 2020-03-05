1b96c37accd7126894793f8d65b8d74886e39a79
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-03-05 20:21:16 +0800

    Create common.pl

---
+++ tool/common.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+
+open( my $in1, "<", $ARGV[0] );
+open( my $in2, "<", $ARGV[1] );
+open( my $in3, "<", $ARGV[2] );
+
+my %count;
+my %info;
+while (<$in1>) {
+    chomp;
+    my ( $chr, $pos, $dir, $base, $gene ) = split /\t/;
+    if ( exists( $count{ $chr . "\t" . $dir . "\t" . $pos } ) ) {
+        $count{ $chr . "\t" . $dir . "\t" . $pos }++;
+    }
+    else {
+        $count{ $chr . "\t" . $dir . "\t" . $pos } = 1;
+        $info{ $chr . "\t" . $dir . "\t" . $pos } =
+          $chr . "\t" . $dir . "\t" . $pos . "\t" . $base . "\t" . $gene;
+    }
+}
+while (<$in2>) {
+    chomp;
+    my ( $chr, $pos, $dir, $base, $gene ) = split /\t/;
+    if ( exists( $count{ $chr . "\t" . $dir . "\t" . $pos } ) ) {
+        $count{ $chr . "\t" . $dir . "\t" . $pos }++;
+    }
+    else {
+        $count{ $chr . "\t" . $dir . "\t" . $pos } = 1;
+        $info{ $chr . "\t" . $dir . "\t" . $pos } =
+          $chr . "\t" . $dir . "\t" . $pos . "\t" . $base . "\t" . $gene;
+    }
+}
+while (<$in3>) {
+    chomp;
+    my ( $chr, $pos, $dir, $base, $gene ) = split /\t/;
+    if ( exists( $count{ $chr . "\t" . $dir . "\t" . $pos } ) ) {
+        $count{ $chr . "\t" . $dir . "\t" . $pos }++;
+    }
+    else {
+        $count{ $chr . "\t" . $dir . "\t" . $pos } = 1;
+        $info{ $chr . "\t" . $dir . "\t" . $pos } =
+          $chr . "\t" . $dir . "\t" . $pos . "\t" . $base . "\t" . $gene;
+    }
+}
+
+foreach ( keys(%count) ) {
+    if ( $count{$_} == 3 ) {
+        print("$info{$_}\n");
+    }
+}
+
+__END__
