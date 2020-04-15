8780f66815b42b579624f8fe77e5da0b9cdcb797
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-04-15 11:20:23 +0800

    Update fetch_fasta.pl

--- tool/fetch_fasta.pl
+++ tool/fetch_fasta.pl
@@ -3,10 +3,6 @@ use warnings;
 
 use Getopt::Long;
 
-#---------------#
-# GetOpt section
-#---------------#
-
 =head1 NAME
 fetch_fasta.pl -- Get all sequences with the same searched string in a FastA file.
 =head1 SYNOPSIS
@@ -16,6 +12,7 @@ fetch_fasta.pl -- Get all sequences with the same searched string in a FastA fil
             --string\-s The sequences we want to fetch
             --file\-f   The FastA file with path
             --stdin     Get FastA from STDIN. It will not been not valid with a provided '--file'
+            --rna2dna   Change "U" to "T". Default: False
 =cut
 
 Getopt::Long::GetOptions(
@@ -23,7 +20,13 @@ Getopt::Long::GetOptions(
     'string|s=s' => \my $char,
     'file|f=s'   => \my $in_fa,
     'stdin'      => \my $stdin,
+    'rna2dna'    => \my $rna2dna,
 ) or Getopt::Long::HelpMessage(1);
+
+sub SEQ_TR_TU {
+    my $SEQ = shift;
+    return ( $SEQ =~ tr/Uu/Tt/r );
+}
 
 my @info;
 if ( defined($in_fa) ) {
@@ -44,6 +47,9 @@ while ( $i <= $#info ) {
         print( $info[$i] );
         my $j = 1;
         until ( ( $i + $j > $#info ) or ( $info[ $i + $j ] =~ /^>/ ) ) {
+            if ( $rna2dna eq "T" ) {
+                $info[ $i + $j ] = SEQ_TR_TU( $info[ $i + $j ] );
+            }
             print( $info[ $i + $j ] );
             $j++;
         }
