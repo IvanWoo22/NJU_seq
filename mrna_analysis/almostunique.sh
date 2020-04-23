6b45d0a76c005564be9086e44e0ef2c4eec43fd2
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-04-23 15:00:07 +0800

    Debug.

--- mrna_analysis/almostunique.sh
+++ mrna_analysis/almostunique.sh
@@ -6,7 +6,7 @@ filepath=$(
 perl "${filepath}"/uniquematch.pl "$1" \
   "$3"/mrna.uniqe.tmp \
   "$3"/mrna.multi.tmp
-perl ~/2OMG/mrna_analysis/almostuniquematch.pl "$2" \
+perl "${filepath}"/almostuniquematch.pl "$2" \
   "$3"/mrna.multi.tmp \
   "$3"/mrna.almost.tmp
 cat "$3"/mrna.almost.tmp \
