c58373e887705fa9f3354bdfa279e668c258e7d6
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-05-27 12:54:09 +0800

    Speed up uniquematch.pl.

--- mrna_analysis/uniquematch.pl
+++ mrna_analysis/uniquematch.pl
@@ -7,7 +7,7 @@ open( my $in_sam1, "<", $ARGV[0] );
 my %list;
 while (<$in_sam1>) {
     chomp;
-    my ( $id, undef ) = split( /\t/, $_, 2 );
+    my $id = split( /\t/, $_, 2 );
     if ( exists( $list{$id} ) ) {
         $list{$id}++;
     }
@@ -17,26 +17,16 @@ while (<$in_sam1>) {
 }
 close($in_sam1);
 
-my ( %uniq_id, %multi_id );
-foreach my $k ( keys(%list) ) {
-    if ( $list{$k} == 1 ) {
-        $uniq_id{$k} = 1;
-    }
-    else {
-        $multi_id{$k} = 1;
-    }
-}
-
 open( my $in_sam2,  "<", $ARGV[0] );
 open( my $out_sam1, ">", $ARGV[1] );
 open( my $out_sam2, ">", $ARGV[2] );
 while (<$in_sam2>) {
     chomp;
-    my ( $id, undef ) = split( /\t/, $_, 2 );
-    if ( exists( $uniq_id{$id} ) ) {
+    my $id = split( /\t/, $_, 2 );
+    if ( $list{$id} == 1 ) {
         print $out_sam1 "$_\n";
     }
-    elsif ( exists( $multi_id{$id} ) ) {
+    else {
         print $out_sam2 "$_\n";
     }
 }
