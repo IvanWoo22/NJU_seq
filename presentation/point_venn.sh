2ec6406b7a3f07511e3562aa5c3d20f887b98044
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-06-07 22:56:37 +0800

    Debug.

--- presentation/point_venn.sh
+++ presentation/point_venn.sh
@@ -4,9 +4,9 @@ filepath=$(
   pwd -P
 )
 
-sort -nrk "$3","$3" "$2" | head -n "${10}" | awk '{print $1 $3 $2}' >templist1.tmp
-sort -nrk "$6","$6" "$5" | head -n "${10}" | awk '{print $1 $3 $2}' >templist2.tmp
-sort -nrk "$9","$9" "$8" | head -n "${10}" | awk '{print $1 $3 $2}' >templist3.tmp
+sort -nrk "$3","$3" "$2" | head -n "${11}" | awk '{print $1 $3 $2}' >templist1.tmp
+sort -nrk "$6","$6" "$5" | head -n "${11}" | awk '{print $1 $3 $2}' >templist2.tmp
+sort -nrk "$9","$9" "$8" | head -n "${11}" | awk '{print $1 $3 $2}' >templist3.tmp
 
-Rscript "${filepath}"/point_venn.R "$1" templist1.tmp "$4" templist2.tmp "$7" templist3.tmp "$7"
-rm templist1.tmp templist2.tmp templist3.tmp "$7"*.log
+Rscript "${filepath}"/point_venn.R "$1" templist1.tmp "$4" templist2.tmp "$7" templist3.tmp "${10}"
+rm templist1.tmp templist2.tmp templist3.tmp "${10}"*.log
