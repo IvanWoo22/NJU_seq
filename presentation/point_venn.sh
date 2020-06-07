6965333ba6160a2111cf6cf4f0ec5f5b16ab3433
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-06-07 22:53:13 +0800

    Debug.

--- presentation/point_venn.sh
+++ presentation/point_venn.sh
@@ -8,5 +8,5 @@ sort -nrk "$3","$3" "$2" | head -n "${10}" | awk '{print $1 $3 $2}' >templist1.t
 sort -nrk "$6","$6" "$5" | head -n "${10}" | awk '{print $1 $3 $2}' >templist2.tmp
 sort -nrk "$9","$9" "$8" | head -n "${10}" | awk '{print $1 $3 $2}' >templist3.tmp
 
-Rscript "${filepath}"/point_draw.R "$1" templist1.tmp "$4" templist2.tmp "$7" templist3.tmp "$7"
+Rscript "${filepath}"/point_venn.R "$1" templist1.tmp "$4" templist2.tmp "$7" templist3.tmp "$7"
 rm templist1.tmp templist2.tmp templist3.tmp "$7"*.log
