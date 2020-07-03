d7bd14793c6f260a3b94d5be91d08e3804ef5bce
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-07-03 18:01:43 +0800

    Output optimization.

--- mrna_analysis/judge_region.pl
+++ mrna_analysis/judge_region.pl
@@ -2,12 +2,11 @@
 use strict;
 use warnings;
 use autodie;
-
 use AlignDB::IntSpan;
 
 my ( %cse, %csi, %ase );
-open( my $ALTERSPLICEGENE, "<", $ARGV[0] );
-while (<$ALTERSPLICEGENE>) {
+open( my $ALTERSPLICE_GENE, "<", $ARGV[0] );
+while (<$ALTERSPLICE_GENE>) {
     chomp;
     my ( $chr, undef, $dir, $constant_exon, $constant_intron, $variable_area )
       = split /\t/;
@@ -32,24 +31,65 @@ while (<$ALTERSPLICEGENE>) {
         $ase{$chr_dir} = AlignDB::IntSpan->new($variable_area);
     }
 }
-close($ALTERSPLICEGENE);
+close($ALTERSPLICE_GENE);
+
+my ( %utr5, %cds, %utr3 );
+open( my $TRANSCRIPT_REGION_GENE, "<", $ARGV[1] );
+while (<$TRANSCRIPT_REGION_GENE>) {
+    chomp;
+    my ( $chr, undef, $dir, $constant_exon, $constant_intron, $variable_area )
+      = split /\t/;
+    $chr =~ s/chr//;
+    my $chr_dir = $chr . "\t" . $dir;
+    if ( exists( $utr5{$chr_dir} ) ) {
+        $utr5{$chr_dir}->AlignDB::IntSpan::add_runlist($constant_exon);
+    }
+    else {
+        $utr5{$chr_dir} = AlignDB::IntSpan->new($constant_exon);
+    }
+    if ( exists( $cds{$chr_dir} ) ) {
+        $cds{$chr_dir}->AlignDB::IntSpan::add_runlist($constant_intron);
+    }
+    else {
+        $cds{$chr_dir} = AlignDB::IntSpan->new($constant_intron);
+    }
+    if ( exists( $utr3{$chr_dir} ) ) {
+        $utr3{$chr_dir}->AlignDB::IntSpan::add_runlist($variable_area);
+    }
+    else {
+        $utr3{$chr_dir} = AlignDB::IntSpan->new($variable_area);
+    }
+}
+close($TRANSCRIPT_REGION_GENE);
 
 while (<STDIN>) {
     chomp;
     my ( $chr, $point, $dir ) = split /\t/;
     my $chr_dir = $chr . "\t" . $dir;
     my $set     = AlignDB::IntSpan->new($point);
-    my $reg     = 0;
+    my $reg1    = 0;
     if ( ( exists( $cse{$chr_dir} ) ) and ( $set->subset( $cse{$chr_dir} ) ) ) {
-        $reg = "CSE";
+        $reg1 = "CSE";
     }
     if ( ( exists( $csi{$chr_dir} ) ) and ( $set->subset( $csi{$chr_dir} ) ) ) {
-        $reg = "CSI";
+        $reg1 = "CSI";
     }
     if ( ( exists( $ase{$chr_dir} ) ) and ( $set->subset( $ase{$chr_dir} ) ) ) {
-        $reg = "ASE";
+        $reg1 = "ASE";
     }
-    print("$_\t$reg\n");
+    my $reg2 = 0;
+    if ( ( exists( $utr5{$chr_dir} ) ) and ( $set->subset( $utr5{$chr_dir} ) ) )
+    {
+        $reg2 = "UTR5";
+    }
+    if ( ( exists( $utr3{$chr_dir} ) ) and ( $set->subset( $utr3{$chr_dir} ) ) )
+    {
+        $reg2 = "UTR3";
+    }
+    if ( ( exists( $cds{$chr_dir} ) ) and ( $set->subset( $cds{$chr_dir} ) ) ) {
+        $reg2 = "CDS";
+    }
+    print("$_\t$reg1\t$reg2\n");
 }
 
 __END__
