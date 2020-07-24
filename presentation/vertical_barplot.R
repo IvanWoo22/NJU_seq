00548183dc250ef3a4dacf2662fd69ce414c25ee
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-07-24 16:58:13 +0800

    Adjust the vertical_barplot.R.

--- presentation/vertical_barplot.R
+++ presentation/vertical_barplot.R
@@ -20,9 +20,8 @@ names(color_scale) <- c("2", "1", "0")
 
 color_scale_point <- c("#fff173","#a7d1b8", "#cccccc")
 names(color_scale_point) <- c("2", "1", "0")
-df_plot %>%
-  mutate(name = fct_reorder(name, log_ave)) %>%
-  ggplot() +
+
+ggplot(mutate(df_plot, name = fct_reorder(name, log_ave))) +
   geom_bar(aes(x = name, y = log_ave, fill= color_pattern), stat="identity", alpha=.6, width = .65, show.legend = F) +
   scale_fill_manual(values = color_scale) +
   expand_limits(y = 0) +
@@ -30,12 +29,12 @@ df_plot %>%
   geom_point(aes(x = name, y = log_num1), shape = 16, color = "#489DCC", alpha = .9, size = 1.2) +
   geom_point(aes(x = name, y = log_num2), shape = 16, color = "#C6175A", alpha = .9, size = 1.2) +
   geom_point(aes(x = name, y = log_num3), shape = 16, color = "#EF7E5B", alpha = .9, size = 1.2) +
-  coord_flip(ylim = c(0, log10(max(raw_data[,4:6])*1.1)) +
+  coord_flip(ylim = c(0, log10(max(raw_data[,4:6])*1.5))) +
   xlab("") +
   ylab("") +
   theme_bw() +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-nrow <- nrow(df_plot)
-height <- 0.525 + nrow * 0.169
+rownum <- nrow(df_plot)
+height <- 0.525 + rownum * 0.169
 ggsave(paste0(result_save_path, ".pdf"), device = NULL, width = 6, height = height, dpi = "retina")
