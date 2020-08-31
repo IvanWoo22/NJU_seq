66aaafb7579cc60fd4146532ba1df6b33ca78549
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2020-08-31 21:23:02 +0800

    New score system for mRNA.

--- mrna_analysis/score_neo.pl
+++ mrna_analysis/score_neo.pl
@@ -8,11 +8,10 @@ my %NC_info;
 open( my $IN_NC, "<", $ARGV[0] );
 while (<$IN_NC>) {
     chomp;
-    my ( $chr, $pos, $dir, undef, undef, undef, undef, undef, $end_count ) =
-      split /\t/;
-    my $id = $chr . "\t" . $dir . "\t" . $pos;
-    $NC_info{$id}  = $_;
-    $NC_count{$id} = $end_count;
+    my @tmp = split /\t/;
+    my $id  = $tmp[0] . "\t" . $tmp[2] . "\t" . $tmp[1];
+    $NC_info{$id}  = join( "\t", @tmp[ 0 .. 6 ] );
+    $NC_count{$id} = $tmp[8];
 }
 close($IN_NC);
 
@@ -21,11 +20,10 @@ my %TR_info;
 open( my $IN_TR, "<", $ARGV[1] );
 while (<$IN_TR>) {
     chomp;
-    my ( $chr, $pos, $dir, undef, undef, undef, undef, undef, $end_count ) =
-      split /\t/;
-    my $id = $chr . "\t" . $dir . "\t" . $pos;
-    $TR_info{$id}  = $_;
-    $TR_count{$id} = $end_count;
+    my @tmp = split /\t/;
+    my $id  = $tmp[0] . "\t" . $tmp[2] . "\t" . $tmp[1];
+    $TR_info{$id}  = join( "\t", @tmp[ 0 .. 6 ] );
+    $TR_count{$id} = $tmp[8];
 }
 close($IN_TR);
 
@@ -40,14 +38,19 @@ foreach my $id ( keys(%TR_count) ) {
     }
 
     my ( $T_END, $T_END_P1, $N_END, $N_END_P1 );
-    unless ( exists( $TR_count{ $chr . "\t" . $dir . "\t" . $formal_pos } ) ) {
-        $TR_count{ $chr . "\t" . $dir . "\t" . $formal_pos } = 1;
+    if ( not exists( $TR_count{ $chr . "\t" . $dir . "\t" . $formal_pos } ) ) {
+        $T_END = 1;
     }
-    unless ( exists( $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos } ) ) {
-        $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos } = 1;
+    else {
+        $T_END = $TR_count{ $chr . "\t" . $dir . "\t" . $formal_pos };
     }
-    $T_END = $TR_count{ $chr . "\t" . $dir . "\t" . $formal_pos };
-    $N_END = $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos };
+    if ( not exists( $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos } ) ) {
+        $N_END = 1;
+    }
+    else {
+        $N_END = $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos };
+    }
+
     if ( ( $T_END < 4 ) and ( $N_END < 4 ) ) {
         if ( $T_END > $N_END ) {
             $N_END = $T_END;
@@ -56,15 +59,17 @@ foreach my $id ( keys(%TR_count) ) {
             $T_END = $N_END;
         }
     }
-    unless ( exists( $NC_count{$id} ) ) {
-        $NC_count{$id} = 0;
+    if ( not exists( $NC_count{$id} ) ) {
+        $N_END_P1 = 0;
+    }
+    else {
+        $N_END_P1 = $NC_count{$id};
     }
     $T_END_P1 = $TR_count{$id};
-    $N_END_P1 = $NC_count{$id};
     $score    = $T_END_P1 / $T_END - $N_END_P1 / $N_END;
 
     if ( $score >= 10 ) {
-        print("$TR_info{$id}\t$score\n");
+        print("$TR_info{$id}\t$T_END_P1\t$T_END\t$N_END_P1\t$N_END\t$score\n");
     }
 }
 
