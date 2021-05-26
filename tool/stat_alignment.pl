0e5ccbd03097d9285ea619b244c1a180cd135cc5
Ivan <wuyifanwd@hotmail.com>
2021-05-26 21:59:17 +0800

    Modify the stat of alignment.

--- tool/stat_alignment.pl
+++ tool/stat_alignment.pl
@@ -6,7 +6,8 @@ use autodie;
 foreach my $filename (@ARGV) {
     open( my $IN, "<", $filename );
     my @tmp  = split "\/", $filename;
-    my $name = $tmp[-1];
+    my @ttmp = split "\.", $tmp[-1];
+    my $name = $ttmp[0];
     my ( $total_reads, $aligned_reads ) = ( 0, 0 );
     my ( $time, $rate );
     while (<$IN>) {
