b3e3305c14411ca309fb04271e01188f46f61ff2
Ivan <wuyifanwd@hotmail.com>
2021-05-26 14:06:36 +0800

    Daily modify for code.

--- mrna_analysis/score.pl
+++ mrna_analysis/score.pl
@@ -2,28 +2,30 @@
 use strict;
 use warnings;
 use autodie;
+use POSIX;
 
 my %NC_count;
-my %NC_info;
+my $NC_total = 0;
 open( my $IN_NC, "<", $ARGV[0] );
 while (<$IN_NC>) {
     chomp;
     my @tmp = split /\t/;
     my $id  = $tmp[0] . "\t" . $tmp[2] . "\t" . $tmp[1];
-    $NC_info{$id}  = join( "\t", @tmp[ 0 .. 6 ] );
     $NC_count{$id} = $tmp[8];
+    $NC_total += $tmp[8];
 }
 close($IN_NC);
 
-my %TR_count;
-my %TR_info;
+my ( %TR_count, %info );
+my $TR_total = 0;
 open( my $IN_TR, "<", $ARGV[1] );
 while (<$IN_TR>) {
     chomp;
     my @tmp = split /\t/;
     my $id  = $tmp[0] . "\t" . $tmp[2] . "\t" . $tmp[1];
-    $TR_info{$id}  = join( "\t", @tmp[ 0 .. 6 ] );
+    $info{$id}     = join( "\t", @tmp[ 0 .. 6 ] );
     $TR_count{$id} = $tmp[8];
+    $TR_total += $tmp[8];
 }
 close($IN_TR);
 
@@ -48,7 +50,10 @@ foreach my $id ( keys(%TR_count) ) {
         $N_END = 1;
     }
     else {
-        $N_END = $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos };
+        $N_END =
+          POSIX::ceil( $NC_count{ $chr . "\t" . $dir . "\t" . $formal_pos } *
+              $TR_total /
+              $NC_total );
     }
 
     if ( ( $T_END < 4 ) and ( $N_END < 4 ) ) {
@@ -63,13 +68,13 @@ foreach my $id ( keys(%TR_count) ) {
         $N_END_P1 = 0;
     }
     else {
-        $N_END_P1 = $NC_count{$id};
+        $N_END_P1 = POSIX::ceil( $NC_count{$id} * $TR_total / $NC_total );
     }
     $T_END_P1 = $TR_count{$id};
     $score    = $T_END_P1 / $T_END - $N_END_P1 / $N_END;
 
     if ( ( $score >= 10 ) and ( $T_END_P1 > $N_END_P1 * 5 ) ) {
-        print("$TR_info{$id}\t$T_END_P1\t$T_END\t$N_END_P1\t$N_END\t$score\n");
+        print("$info{$id}\t$T_END_P1\t$T_END\t$N_END_P1\t$N_END\t$score\n");
     }
 }
 
